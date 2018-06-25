25403c150c301d2cf7bedc4bca6df771f77a1277
Jorge Trincado <jatrincado@uc.cl>
2018-06-25 19:20:04 -0400

    added cdn socket

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+//var io = require('socket.io')(http);
 
-app.set('port', (process.env.PORT || 8080))
+
+//app.set('port', (process.env.PORT || 8080))
+app.set('port', 3000)
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-  console.log('Browser conectado');
-
-  socket.on('data', function(data){
-    console.log(data);
-  });
-
-  socket.on('disconnect', function(){
-    console.log('Browser desconectado');
-  });
 });
 
 app.listen(app.get('port'));
